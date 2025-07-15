ad92ebe3b29fba66f5b866adfbc5302345c3a3f5
bikoum_s <bikoum_s@etna-alternance.net>
2025-07-16 01:06:15 +0200

    modicontactcss

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,18 @@
+import styles from "../styles/Contact.module.css";
+
 export default function Contact() {
   return (
-    <section id="contact" className="contactContainer">
-      <h2 className="contactTitle">Me Contacter</h2>
-      <p className="contactDescription">
+    <section id="contact" className={styles.contactContainer}>
+      <h2 className={styles.contactTitle}>Me Contacter</h2>
+      <p className={styles.contactDescription}>
         Un projet, une opportunité d'alternance, ou juste l’envie d’échanger sur la tech ?
         Je suis toujours ouvert aux discussions, aux conseils et aux nouvelles rencontres professionnelles.
       </p>
-      <p className="contactDescription">
+      <p className={styles.contactDescription}>
         N’hésitez pas à m’écrire, je répondrai avec plaisir :
       </p>
-      <div className="contactEmail">
-        <a href="mailto:sorenbikouta11@gmail.com"> Me contacter par email</a>
+      <div className={styles.contactEmail}>
+        <a href="mailto:sorenbikouta11@gmail.com">📩 Me contacter par email !!!</a>
       </div>
     </section>
   );
