f5bb7f86dc479fc8eafc492db158c1e41c9c019c
bikoum_s <bikoum_s@etna-alternance.net>
2025-07-16 00:56:24 +0200

    modifpsgcontact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,17 @@
 export default function Contact() {
   return (
-    <section id="contact">
-      <h2>Me contacter</h2>
-      <p>Un projet, une opportunité d'alternance, ou juste l’envie d’échanger sur la tech et le développement ? Je suis toujours ouvert aux discussions, aux conseils et aux nouvelles rencontres professionnelles.</p>
-      <p>N’hésitez pas à m’écrire, je répondrai avec plaisir :</p>
-      <p>Email : <a href="mailto:sorenbikouta11@gmail.com">sorenbikouta11@gmail.com</a></p>
+    <section id="contact" className="contactContainer">
+      <h2 className="contactTitle">Me Contacter</h2>
+      <p className="contactDescription">
+        Un projet, une opportunité d'alternance, ou juste l’envie d’échanger sur la tech ?
+        Je suis toujours ouvert aux discussions, aux conseils et aux nouvelles rencontres professionnelles.
+      </p>
+      <p className="contactDescription">
+        N’hésitez pas à m’écrire, je répondrai avec plaisir :
+      </p>
+      <div className="contactEmail">
+        <a href="mailto:sorenbikouta11@gmail.com"> Me contacter par email</a>
+      </div>
     </section>
   );
 }
